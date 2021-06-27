b10e9c5e284e92507f606adb29bd8d8c2e4c31d7
Chase <soccerchase@gmail.com>
2021-06-27 17:00:23 -0600

    Update

--- combiner.rb
+++ combiner.rb
@@ -2,27 +2,30 @@
 #
 
 require 'optparse'
+require 'io/console'
+require 'pry'
 
 class WordlistParser
   Version = "1.0.0"
 
   OUTPUT = {stdout: true, file: false}
-
+  attr_accessor :file1, :file2, :combined_file
 
   class ScriptOptions
-    attr_accessor :file1, :file2, :combined_file,
 
-    def initialize
+    attr_accessor :verbose, :encoding
+    def initialize(parser)
       self.verbose = true
       self.encoding = "utf8"
+      binding.pry
     end
 
     def define_options(parser)
       parser.banner = "Usage: examble.rb -l1 file1 -l2 file2"
       parser.seperator=""
 
-      file1(parser)
-      file2(parser)
+      self.file1 = file1(parser)
+      self.file2 = file2(parser)
 
        parser.on_tail("--version", "Show version") do
           puts Version
@@ -32,13 +35,13 @@ class WordlistParser
 
     def file1(parser)
       parser.on("-l1", "--file1 FILE1") do file1
-        self.file1 = file1 if File.exist?(file1)
+        return file1
       end
     end
 
     def file2(parser)
       parser.on("-l2", "--file2 FILE2") do file2
-        self.file2 = file2 if File.exist?(file2)
+        return file2
       end
     end
   end
@@ -51,9 +54,24 @@ class WordlistParser
     end
     @options
   end
+
+  def generate_new
+    file = File.new( "output-wordlist.txt", "w")
+    file.chmod( 0755 )
+    IO.foreach(self.file1) do |l|
+      IO.foreach(self.file2) do |w|
+        file.write(l + ':' + w + '\n')
+      end
+    end
+  file.close
+  end
+
 end # Class WordlistParser
 
-wordlist = WordlistParser.new
+wordlist = WordlistParser.new(
 options = wordlist.parse(ARGV)
-print options
-print ARGV
+puts wordlist.file1
+puts wordlist.file2
+
+wordlist.generate_new
+
